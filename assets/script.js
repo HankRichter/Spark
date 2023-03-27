14615b099d7b4803b7d0a552032abf0c5e628eb5
Hank Richter <120127039+HankRichter@users.noreply.github.com>
2023-03-26 23:36:27 -0400

    HTML changes and JS written to display local storage

--- assets/script.js
+++ assets/script.js
@@ -8,21 +8,83 @@ const hero = document.querySelector(".hero");
 const quote1 = document.getElementById("quote");
 const image = document.getElementById("image");
 const save = document.getElementById("save");
-const goBack = document.getElementById("goback");
+const goBack = document.querySelector(".goback");
 const oneDeeTwenty = [Math.floor(Math.random() * 20)];
 const quoteArray = "https://type.fit/api/quotes";
 let favoriteCombinations =
   JSON.parse(localStorage.getItem("picture-quote")) || [];
+const quoteImageLS = document.querySelector(".quote-image-LS");
 
 const pictureQuote = {
   chosenImage: null,
   chosenQuote: null,
 };
 
+function favorites() {
+  hero.style.display = "none";
+  quoteImageLS.style.display = "block";
+  favoriteCombinations.forEach((element) => {
+    const div = document.createElement("div");
+    const img = document.createElement("img");
+    const div2 = document.createElement("div");
+    const ptag = document.createElement("p");
+    const div3 = document.createElement("div");
+    const button = document.createElement("button");
+    div.classList.add("relative");
+    img.classList.add(
+      "relative",
+      "isolate",
+      "overflow-hidden",
+      "bg-gray-900",
+      "py-24",
+      "sm:py-32",
+      "h-screen",
+      "w-screen",
+      "object-contain"
+    );
+    div2.classList.add(
+      "flex",
+      "flex-col",
+      "justify-center",
+      "m-auto",
+      "w-full",
+      "absolute",
+      "sm:bottom-[80px]",
+      "bottom-[120px]"
+    );
+    ptag.classList.add(
+      "flex",
+      "justify-center",
+      "m-auto",
+      "w-full",
+      "text-white"
+    );
+    div3.classList.add(
+      "flex",
+      "justify-center",
+      "m-auto",
+      "w-full",
+      "text-sm",
+      "sm:text-base"
+    );
+    button.classList.add("btn");
+    quoteImageLS.appendChild(div);
+    div.appendChild(img);
+    div.appendChild(div2);
+    div2.appendChild(ptag);
+    div2.appendChild(div3);
+    div3.appendChild(button);
+    img.src = element.chosenImage;
+    ptag.textContent = element.chosenQuote;
+    button.textContent = "Go Back!";
+    button.addEventListener("click", function () {
+      hero.style.display = "block";
+      quoteImageLS.style.display = "none";
+    });
+  });
+}
+
 function postToLocalStorage() {
-  for (let i = 0; i < favoriteCombinations.length; i++) {
-    const LSBtn = document.createElement("button");
-  }
   if (favoriteCombinations) {
     favoriteCombinations.push(pictureQuote);
     localStorage.setItem("picture-quote", JSON.stringify(favoriteCombinations));
@@ -45,7 +107,6 @@ function imageFetch() {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
       const chosenImageURL = data.hits[oneDeeTwenty].largeImageURL;
       pictureQuote.chosenImage = chosenImageURL;
       console.log(pictureQuote.chosenImage);
@@ -59,7 +120,6 @@ function quoteFetch() {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
       hero.style.display = "none";
       quoteImage.style.display = "block";
       const quote =
@@ -84,3 +144,5 @@ goBack.addEventListener("click", function () {
   hero.style.display = "block";
   quoteImage.style.display = "none";
 });
+
+LSBtn.addEventListener("click", favorites);
